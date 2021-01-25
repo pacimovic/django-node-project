817cdedc4d24adfe620e6030af59795b337996b1
pera98 <petaracimovic990@gmail.com>
2021-01-25 15:00:25 +0100

    Post django app

---
+++ django app/accounts/views.py
@@ -0,0 +1,82 @@
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
+from .forms import CreateNewUser, CheckUser
+
+# Create your views here.
+
+def login(request):
+    if(request.method=='POST'):
+        form = CheckUser(request.POST)
+        if form.is_valid():
+            username=form.cleaned_data['username']
+            password=form.cleaned_data['password']
+
+            user =auth.authenticate(username=username,password=password)
+            if(user is not None):
+                auth.login(request,user)
+                nxt = request.GET.get("next", None)
+                if nxt is None:
+                    messages.info(request,'Login successful')
+                    return redirect("/")
+                else:
+                    if not user.is_superuser and nxt == '/add':
+                        messages.info(request,'Your account doesnt have access to this page')
+                    return redirect(nxt)
+            else:
+                messages.info(request,'Invalid username or password')
+                return render(request,'login.html',{"form":form})
+
+    else:
+        nxt = request.GET.get("next", None)
+        if nxt is not None:
+            messages.info(request,'Please login to see this page.')
+        form = CheckUser()
+        return render(request,'login.html',{"form":form})
+
+
+def register(request):
+
+    if(request.method=='POST'):
+        
+        form = CreateNewUser(request.POST)
+
+        if form.is_valid():
+            first_name=form.cleaned_data['first_name']
+            last_name=form.cleaned_data['last_name']
+            username=form.cleaned_data['username']
+            password1=form.cleaned_data['password1']
+            password2=form.cleaned_data['password2']
+            email=form.cleaned_data['email']
+
+            if(password1==password2):
+                if(User.objects.filter(username=username).exists()):
+                    messages.info(request,'Username taken')
+                    return render(request,'register.html',{"form":form})
+                elif(User.objects.filter(email=email).exists()):
+                    messages.info(request,'email taken')
+                    return render(request,'register.html',{"form":form})
+                else:    
+                    user=User.objects.create_user(username=username,password=password1,email=email,first_name=first_name,last_name=last_name)
+                    user.save()
+                    print('user created')
+                    messages.info(request,'Register successful')
+                    return redirect('login')
+            else:
+                messages.info(request,'password not matching...')
+                return render(request,'register.html',{"form":form})
+            
+            return redirect('/')
+
+        else:
+            messages.info(request,'Forma nije validna!')
+            return render(request,'register.html')
+    else:
+        form = CreateNewUser()
+        return render(request,'register.html',{"form":form})
+
+
+def logout(request):
+    auth.logout(request)
+    messages.info(request,'Logout successful')
+    return redirect('/')
---
+++ django app/dingo/views.py
@@ -0,0 +1,95 @@
+import os
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from django.conf import settings
+from .models import Food
+from django.core.files.storage import FileSystemStorage
+from django.contrib import messages
+from .forms import AddNewFood
+from django.shortcuts import get_object_or_404
+from django.contrib.auth.decorators import login_required, permission_required
+
+
+# Create your views here.
+
+def index(request):
+
+    foods=Food.objects.all()
+    return render(request,"index.html",{'foods' : foods})
+
+
+def search(request):
+    
+    foods=Food.objects.all()
+    
+    newFoods=[]
+    search=request.GET["search"]
+    if(search!=""): 
+        for food in foods:
+            if(food.name==search):
+                newFoods.append(food)
+            elif(str(food.price)==search):
+                newFoods.append(food)
+        foods=newFoods 
+        if not foods:
+            messages.info(request,'Food not find!') 
+
+    return render(request,"index.html",{'foods' : foods})
+
+
+@login_required
+def detail(request, detail):
+    food = get_object_or_404(Food, id=detail)
+        
+    return render(request,"detail.html",{'food':food})
+
+
+@permission_required('domaci.add_food')
+def add(request):        
+    if(request.method=='POST'):
+
+        form = AddNewFood(request.POST,request.FILES)
+
+        if form.is_valid():
+            name=form.cleaned_data['name']
+            img=form.cleaned_data['img']
+            desc=form.cleaned_data['desc']
+            price=form.cleaned_data['price']
+
+            try:
+                pass
+                tmp = int(price)
+            except ValueError:
+                messages.info(request,'Mora Broj!')
+                return render(request,"add.html",{'form':form})
+            else:
+                price=tmp
+                #fs=FileSystemStorage()
+                #fs.save(img.name,img)
+
+                newFood=Food(name=name,img=img,desc=desc,price=price)
+                newFood.save()
+            
+            messages.info(request,'Food created successful!')
+            return redirect('/')
+        else:
+            #nije validna forma
+            messages.info(request,'nije validna forma')
+            return render(request,"add.html",{'form':form})
+    else:
+        form = AddNewFood()
+        return render(request,"add.html",{'form':form})
+
+
+@permission_required('domaci.delete_food')
+def delete(request, id):
+
+    food = get_object_or_404(Food, id=id)
+    if food is not None:
+        fs=FileSystemStorage()
+        fs.delete(food.img.name)
+        food.delete()
+
+
+    return redirect('/')
+
